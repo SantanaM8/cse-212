e830808a12ce344e8edcd1873f58f9c904f253aa
SantanaM8 <134400377+SantanaM8@users.noreply.github.com>
2026-09-26 15:32:42 -0300

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,29 @@
-public class FeatureCollection
+public static async Task<string[]> EarthquakeDailySummary()
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    const string url =
+        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+    using var client = new HttpClient();
+
+    string json = await client.GetStringAsync(url);
+
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    var featureCollection =
+        JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+    var results = new List<string>();
+
+    foreach (var feature in featureCollection.Features)
+    {
+        string place = feature.Properties.Place;
+        double? magnitude = feature.Properties.Mag;
+
+        results.Add($"{place} - Mag {magnitude}");
+    }
+
+    return results.ToArray();
 }
